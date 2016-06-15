b3c0f0b509de80134dc3ad58eff2d040284e3e29
KiaraRaynS <sherman.rayn@outlook.com>
2016-06-15 15:16:18 -0400

    Create user page added

--- calcapp/views.py
+++ calcapp/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 
 # Create your views here.
+
+
+def view_index(request):
+    return render(request, 'index.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def use_calc(request):
+    return render(request, 'calculator.html')
+
+
+def create_user(request):
+    if request.POST:
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, 'createuser.html', {'form': form})
+    form = UserCreationForm()
+    return render(request, 'createuser.html', {'form': form})
--- logincalc/urls.py
+++ logincalc/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from calcapp.views import view_index, login, use_calc, create_user
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', view_index, name='index'),
+    url(r'^login/$', login, name='login'),
+    url(r'^/user/usecalc', use_calc, name="usecalc"),
+    url(r'^createuser/$', create_user, name="createuser")
 ]
